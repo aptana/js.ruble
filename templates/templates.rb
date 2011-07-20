c292be2e5e1e84b6e51fae50aa16ca14dbb2d284
Allen Yeung <ayeung@aptana.com>
2011-07-20 14:22:48 -0700

    Fix APSTUD-2134:Add .location property to file templates

--- templates/templates.rb
+++ templates/templates.rb
@@ -2,9 +2,5 @@ require 'ruble'
 
 template "JavaScript Template" do |t|
   t.filetype = "*.js"
-  t.invoke do |context|
-    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.js")
-    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
- end
+  t.location = "templates/template.js"
 end
